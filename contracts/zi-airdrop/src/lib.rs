89aaeb014f814d663ff28791393ca4eedf11dbbd
best-journey <zxcvwert3458@gmail.com>
2025-02-18 19:21:36 +0000

    feat: update-airdrop-contract

--- contracts/zi-airdrop/src/lib.rs
+++ contracts/zi-airdrop/src/lib.rs
@@ -1,5 +1,12 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Address, ConversionError, Env, TryFromVal, Val};
+use soroban_sdk::{contract, contractimpl, contracterror, token::TokenClient, Address, ConversionError, Env, TryFromVal, Val};
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    AlreadyReceived = 1,
+}
 
 #[derive(Clone, Copy, Debug)]
 pub enum Action {
@@ -37,42 +44,25 @@ pub struct ZiAirdrop;
 
 #[contractimpl]
 impl ZiAirdrop {
-    pub fn distribute_tokens(e: &Env, user: Address, action: Action) {
-        // Check if the action is valid and has not been performed before
-        if Self::has_performed_action(e, &user, action) {
-            panic!("You have already received this type of airdrop.");
+    pub fn distribute_tokens(e: Env, token_id: Address, sender: Address, recipient: Address, action: Action) -> Result<bool, Error> {
+        if Self::has_performed_action(&e, &recipient, action) {
+            return Err(Error::AlreadyReceived);
         }
 
         let reward = match action {
             Action::Unknown => 0,
-            Action::SpinCube => 2,
-            Action::CreateParticles => 2,
-            Action::ChangeTheme => 1,
+            Action::SpinCube => 20000000,
+            Action::CreateParticles => 20000000,
+            Action::ChangeTheme => 10000000,
         };
 
-        // Retrieve the current balance of the user from storage
-        let balance = Self::get_balance(e, user.clone());
+        sender.require_auth();
 
-        // Update the user's balance
-        let new_balance = balance + reward;
-        Self::set_balance(e, user.clone(), new_balance);
+        let token = TokenClient::new(&e, &token_id);
+        token.transfer(&sender, &recipient, &reward);
 
-        // Mark the action as performed
-        Self::mark_action_performed(e, &user, action);
-    }
-
-    pub fn get_balance(e: &Env, addr: Address) -> u128 {
-        // Try to get the balance from storage, default to 0 if not set
-        if let Some(balance) = e.storage().persistent().get(&addr) {
-            balance
-        } else {
-            0
-        }
-    }
-
-    fn set_balance(e: &Env, addr: Address, amount: u128) {
-        // Store the updated balance in storage
-        e.storage().persistent().set(&addr, &amount);
+        Self::mark_action_performed(&e, &recipient, action);
+        Ok(true)
     }
 
     fn has_performed_action(e: &Env, user: &Address, action: Action) -> bool {
@@ -80,16 +70,16 @@ impl ZiAirdrop {
         e.storage().instance().has(&key)
     }
 
-    pub fn get_status(e: &Env, user: Address) -> u128 {
-        if Self::has_performed_action(e, &user, Action::ChangeTheme) {
+    pub fn get_status(e: Env, user: Address) -> u128 {
+        if Self::has_performed_action(&e, &user, Action::ChangeTheme) {
             return 3;
         }
 
-        if Self::has_performed_action(e, &user, Action::CreateParticles) {
+        if Self::has_performed_action(&e, &user, Action::CreateParticles) {
             return 2;
         }
 
-        if Self::has_performed_action(e, &user, Action::SpinCube) {
+        if Self::has_performed_action(&e, &user, Action::SpinCube) {
             return 1;
         }
 
@@ -113,27 +103,5 @@ mod test {
         let e = Env::default();
         let contract_id = e.register_contract(None, ZiAirdrop);
         let client = ZiAirdropClient::new(&e, &contract_id);
-
-        let user = Address::from_str(&e, "GAYW7P7VQ6BDSSW4TJOIXX5CITVFG455NQIAMCK22BYUOUHI6CKOAEFV");
-
-        // Distribute tokens for action 1 (SpinCube)
-        client.distribute_tokens(&user, &Action::SpinCube);
-        assert_eq!(client.get_balance(&user), 2);
-
-        // Try to distribute tokens for action 1 again (should not change balance)
-        client.distribute_tokens(&user, &Action::SpinCube);
-        assert_eq!(client.get_balance(&user), 2);
-
-        // Distribute tokens for action 2 (CreateParticles)
-        client.distribute_tokens(&user, &Action::CreateParticles);
-        assert_eq!(client.get_balance(&user), 4);
-
-        // Distribute tokens for action 3 (ChangeTheme)
-        client.distribute_tokens(&user, &Action::ChangeTheme);
-        assert_eq!(client.get_balance(&user), 5);
-
-        // Try to distribute tokens for action 2 again (should not change balance)
-        client.distribute_tokens(&user, &Action::CreateParticles);
-        assert_eq!(client.get_balance(&user), 5);
     }
 }
